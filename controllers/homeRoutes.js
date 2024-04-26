c2bee50d50b34d06773e5380ce3005b574619ecd
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-04-26 12:46:54 -0400

    user Routes tested good

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 const { User } = require('../models');
+const {Song } = require('../models');
+const {Playlist } = require('../models');
+const withAuth = require('../utils/auth');
+
 
 // TODO: Add a comment describing the functionality of the withAuth middleware
 // checks to see if the user is logged in before allowing them to access the route
@@ -25,9 +29,25 @@ router.get('/faq', (req, res) => {
     res.render('faq', {logged_in: req.session.logged_in});
 });
 
-router.get('/music', (req, res) => {
-    res.render('music', {logged_in: req.session.logged_in});
+router.get('/music',  async (req, res) => {
+    try {
+        // Fetch the user's playlists from the database
+        // const playlists = await Playlist.findAll({
+        //     where: { user_id: req.session.user_id },
+        //     include: ['songs'] // Include the songs in each playlist
+        // });
+
+        // Fetch all songs from the database
+        const songs = await Song.findAll();
+
+        // Render the 'music' view and pass the playlists and songs
+        res.render('music', { songs});
+    } catch (err) {
+        // If there was an error, return a 500 error
+        res.status(500).send(err.message);
+    }
 });
+
 
 
 
