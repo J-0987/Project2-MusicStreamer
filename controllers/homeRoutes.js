af545efe13e142667e2aa5cc0c643e9cac434416
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-04-26 10:59:16 -0400

    restore accidental changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../models');
+const withAuth = require('../utils/auth');
 
 // TODO: Add a comment describing the functionality of the withAuth middleware
 // checks to see if the user is logged in before allowing them to access the route
@@ -26,8 +27,23 @@ router.get('/faq', (req, res) => {
     res.render('faq');
 });
 
-router.get('/music', (req, res) => {
-    res.render('music');
+router.get('/music', withAuth, async (req, res) => {
+    try {
+        // Fetch the user's playlists from the database
+        const playlists = await Playlist.findAll({
+            where: { user_id: req.session.user_id },
+            include: ['songs'] // Include the songs in each playlist
+        });
+
+        // Fetch all songs from the database
+        const songs = await Song.findAll();
+
+        // Render the 'music' view and pass the playlists and songs
+        res.render('music', { playlists, songs, loggedIn: req.session.loggedIn });
+    } catch (err) {
+        // If there was an error, return a 500 error
+        res.status(500).send(err.message);
+    }
 });
 
 
